afbe9c4a4384e1250064b43e7f6fac91902add64
‘andreadioni’ <andreadionim@gmail.com>
2019-09-12 12:46:05 +0200

    hej

--- Personality data.R
+++ Personality data.R
@@ -6,4 +6,5 @@ Shoesize43 <- subset(persData, shoesize ==43)
 Shoesizeunder40 <- subset(persData, shoesize < 40)
 mean(persData$shoesize) #How to find the mean of a column
 AverageShoesize <- mean(persData$shoesize)
-AverageShoesize
+AverageShoesize hej
+
